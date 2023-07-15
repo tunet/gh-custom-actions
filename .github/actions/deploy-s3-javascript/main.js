55a14a409c19b95908eb5d229f943a207a5611ae
aliaksandrkulba <tunet000@gmail.com>
2023-07-15 20:04:38 +0300

    feat: show website url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,6 +9,9 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
+
     core.notice('Hello from my custom JavaScript Action!');
 }
 

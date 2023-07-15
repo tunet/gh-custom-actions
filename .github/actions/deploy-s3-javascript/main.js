21f6785de95b6643a8ab6045aadd61b8b30d5618
aliaksandrkulba <tunet000@gmail.com>
2023-07-15 19:32:19 +0300

    feat: deploy site to S#

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,14 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 function run() {
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('Hello from my custom JavaScript Action!');
 }
 
